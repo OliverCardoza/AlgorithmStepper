d9952a6865118d0f9e84c2ef610b3feed3d33747
Oliver Cardoza <oracardo@gmail.com>
2013-04-29 18:23:32 -0400

    Remove data and algorithm from StepController

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
-requirejs(['StepController'],
-function(   StepController) {
+requirejs(['StepController', 'algorithms/bubbleSort'],
+function(   StepController,   bubbleSort) {
   var playButton = document.getElementById('play');
   var pauseButton = document.getElementById('pause');
   var stopButton = document.getElementById('stop');
 
   var controller;
+  var data = [20, 60, 10, 50, 90, 30]; // TODO: replace with generator
 
   function setNewAlgorithm(name){
     if(controller){
@@ -13,7 +14,16 @@ function(   StepController) {
       pauseButton.removeEventListener('click', controller.pause);
       stopButton.removeEventListener('click', controller.stop);
     }
-    controller = new StepController(name);
+    var actions;
+    switch(name){
+      case 'bubbleSort':
+        actions = bubbleSort(data, true);
+        break;
+      default:
+        throw new Error('Bad algorithm name.');
+    }
+
+    controller = new StepController(data, actions);
     playButton.addEventListener('click', controller.play, false);
     pauseButton.addEventListener('click', controller.pause, false);
     stopButton.addEventListener('click', controller.stop, false);
