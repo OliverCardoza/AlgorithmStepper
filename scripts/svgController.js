14106eda6f1b78cffd9d3932a8a7260dbccc1319
Oliver Cardoza <oracardo@gmail.com>
2013-05-14 17:22:17 -0400

    Changed circles to trapezoids. Broke everything after init. Made a giant mess. Too tired and dizzy to finish

--- scripts/svgController.js
+++ scripts/svgController.js
@@ -10,21 +10,67 @@ function(d3,       _,                constants){
   }
 
   function init(data){
+    var max = _.max(data);
+    var verticalPadding = 30;
+    var verticalMaxHeight = (svg.attr('height')-verticalPadding)/3; // Allow for each element to have at minimum 100% padding above and below
+    var horizontalMiddle = svg.attr('height')/2;
+    var horizontalSpacing = 10;
+    var cornerHeight = 10;
+    var verticalScalingFactor = verticalMaxHeight/(max-1);
     scalingFactor = (svg.attr('width')-100)/data.length; // TODO: magic number
 
     // TODO: define better coordinate system for shifts
     yUnit = 100;
     xUnit = scalingFactor;
 
-    svg.selectAll("circle")
+    // TODO: be able to change sort visualization
+    // svg.selectAll("circle")
+    //     .data(data)
+    //   .enter().append("circle")
+    //     .attr("cy", svg.attr('height')/2)
+    //     .attr("cx", function(val, i){
+    //       return scalingFactor*i+50;
+    //     })
+    //     .attr("r", function(val){
+    //       // return val*3/data.length; // TODO: Magic number
+    //       return (val/max)*0.5*(scalingFactor*0.95);
+    //     })
+    //     .attr("id", function(val){
+    //       return 'd'+val;
+    //     });
+
+    // Assumes numbers are in range [1, max]
+    svg.selectAll("polygon")
         .data(data)
-      .enter().append("circle")
-        .attr("cy", svg.attr('height')/2)
-        .attr("cx", function(val, i){
-          return scalingFactor*i+50;
-        })
-        .attr("r", function(val){
-          return val*3/data.length; // TODO: Magic number
+      .enter().append("polygon")
+        .attr("points", function(val, i){
+          //    p3
+          // p2
+          // p1
+          //    p4
+          var p1y = horizontalMiddle + (val-1)*verticalScalingFactor/2;
+          var p2y = horizontalMiddle - (val-1)*verticalScalingFactor/2;
+          var p3y = p2y - (cornerHeight/2 );
+          var p4y = p1y + (cornerHeight/2 );
+          // var p1y = horizontalMiddle + (val-1)*cornerHeight*2;
+          // var p2y = horizontalMiddle - (val-1)*cornerHeight*2;
+          // var p3y = p2y - (cornerHeight );
+          // var p4y = p1y + (cornerHeight );
+
+          // var p1x = 100 + i*horizontalSpacing;
+          // var p2x = p1x;
+          // var p3x = p1x + horizontalSpacing;
+          // var p4x = p3x;
+
+          var p1x = 100 + i*2*horizontalSpacing;
+          var p2x = p1x;
+          var p3x = p1x + horizontalSpacing;
+          var p4x = p3x;
+          return p1x + "," + p1y + " "
+               + p2x + "," + p2y + " "
+               + p3x + "," + p3y + " "
+               + p3x + "," + p4y;
+
         })
         .attr("id", function(val){
           return 'd'+val;
@@ -35,6 +81,7 @@ function(d3,       _,                constants){
     element.transition().duration(constants.transitionDuration).style('fill', color);
   }
 
+  // TODO: fix all shifts
   function setColorAndShift(element, color, xUnits, yUnits){
     var x = Number(element.attr('cx'));
     var y = Number(element.attr('cy'));
