68ab25d7020d657fff0e6018d61c7e94ff5e3fdd
Oliver Cardoza <oracardo@gmail.com>
2013-05-22 18:09:53 -0400

    Basic quicksort

--- scripts/algorithms/quickSort.js
+++ scripts/algorithms/quickSort.js
@@ -1,154 +1,67 @@
-define(['svgController', 'util/constants'],
-function(svgController,   constants){
-  var actions;
-  var step;
-  var list;
-  var ascending;
+define(['step/StepBuilder', 'util/constants'],
+function(StepBuilder,   constants){
 
-  var current = {};
-  var colors = {
-    pivot: constants.colors.tertiary,
-    primary: constants.colors.primary,
-    secondary: constants.colors.secondary,
-    default: constants.colors.default
-  };
-
-  function addAction(command, params){
-    actions.push(svgController.createAction(step, command, params));
-  }
-
-  function select(type, datum){
-    if(current[type]){
-      deselect(current[type]);
-    }
-    addAction('setColor', {
+  function selectPivot(sb, datum){
+    sb.addAction('setColor', {
       datum: datum,
-      color: colors[type]
+      color: constants.colors.tertiary
     });
-    if(type!=='default'){
-      current[type] = datum;
+  }
+
+  function sortSubarray(list, ascending, sb, leftIndex, rightIndex, pivotIndex){
+    // TODO: select subarray as focus
+    if(leftIndex >= rightIndex){
+      sb.selectSorted(list[leftIndex]);
+      sb.selectSorted(list[rightIndex]);
+      sb.incrementStep();
+      return;
     }
-  }
 
-  function deselect(datum){
-    select('default', datum);
-  }
-  function selectPivot(datum){
-    select('pivot', datum);
-  }
-  function selectSecondary(datum){
-    select('secondary', datum);
-  }
-
-  function selectPrimary(datum){
-    addAction('setColorAndShift', {
-      datum: datum,
-      color: colors['primary'],
-      xUnits: 0,
-      yUnits: -1
-    });
-    current['primary'] = datum;
-  }
-  function deselectPrimary(){
-    addAction('setColorAndShift', {
-      datum: current['primary'],
-      color: colors['default'],
-      xUnits: 0,
-      yUnits: 1
-    });
-    current['primary'] = null;
-  }
-
-  function swap(idx1, idx2){
-    var tmp = list[idx1];
-    list[idx1] = list[idx2];
-    list[idx2] = tmp;
-  }
-
-  function swapPosition(idx1, idx2){
-    addAction('swapHorizontalPosition', [list[idx1], list[idx2]]);
-    swap(idx1, idx2);
-  }
-
-  // function swapColorAndPosition(idx1, idx2){
-  //   addAction('swapColorAndHorizontalPosition', [list[idx1], list[idx2]]);
-  //   swap(idx1, idx2);
-  // }
-
-  // TODO: exit condition on small space
-  function sort2(leftIndex, rightIndex, pivotIndex){
     var primaryIndex = leftIndex;
-    var secondaryIndex;
 
-    // Select pivot and move to the end
-    selectPivot(list[pivotIndex]);
-    step++;
-    if(rightIndex !== pivotIndex){
-      swapPosition(pivotIndex, rightIndex);
-      pivotIndex = rightIndex;
-      step++;
-    }
+    // move pivot to end
+    selectPivot(sb, list[pivotIndex]);
+    sb.incrementStep();
+    sb.swap(list, pivotIndex, rightIndex);
+    pivotIndex = rightIndex;
+    sb.incrementStep();
+    sb.select(list[primaryIndex], 'primary');
 
-    // Select leftmost element as primary
-    selectPrimary(list[primaryIndex]);
-    step++
-
-    for(secondaryIndex = leftIndex, max = rightIndex; secondaryIndex<max; secondaryIndex++){
-      if(secondaryIndex !== primaryIndex){
-        selectSecondary(list[secondaryIndex]);
-        step++
-      }
-
+    for(var secondaryIndex=leftIndex; secondaryIndex<rightIndex && sb.select(list[secondaryIndex], 'secondary'); secondaryIndex++){
+      sb.incrementStep();
       var outOfOrder = (ascending && list[secondaryIndex]<list[pivotIndex]) || (!ascending && list[secondaryIndex]>list[pivotIndex]);
       if(outOfOrder){
-        if(secondaryIndex !== primaryIndex){
-          swapPosition(secondaryIndex, primaryIndex);
-          step++;
-          return;
-          var oldSecondary = secondaryIndex;
-          secondaryIndex = primaryIndex;
-          primaryIndex++;
-          current['primary'] = list[primaryIndex];
-
-          deselect(list[secondaryIndex]);
-          step++;
+        if(primaryIndex !== secondaryIndex){
+          sb.swap(list, primaryIndex, secondaryIndex);
+          sb.incrementStep();
         }
-        // fix primary/secondary pointers and deselect secondary
         
-        
-        deselectPrimary();
-        selectPrimary(list[primaryIndex]);
-        step++;
-      }
-    }
-    swapPosition(primaryIndex, pivotIndex);
-    step++;
-    deselect(list[primaryIndex]);
-    // TODO: stuff here
-  }
-
-  function sortSubarray(leftIndex, rightIndex, pivotIndex){
-    var primaryIndex = leftIndex;
-
-    swap(pivotIndex, rightIndex);
-
-    for(var secondaryIndex=leftIndex, max=rightIndex; secondaryIndex<max; secondaryIndex++){
-      var outOfOrder = (ascending && list[secondaryIndex]<list[pivotIndex]) || (!ascending && list[secondaryIndex]>list[pivotIndex]);
-      if(outOfOrder){
-        swap(primaryIndex, secondaryIndex);
         primaryIndex++;
+        sb.select(list[primaryIndex], 'primary');
       }
     }
+    // Finishing up by swapping pivot into correct spot and selecting it as sorted
+    sb.deselect('secondary');
+    sb.incrementStep();
+    sb.swap(list, primaryIndex, pivotIndex);
+    pivotIndex = primaryIndex;
+    sb.incrementStep();
+    sb.deselect('primary');
+    sb.selectSorted(list[pivotIndex]);
+    sb.incrementStep();
+
+    // TODO change to random pivot
+    // TODO: select all as normal again
+    sortSubarray(list, ascending, sb, leftIndex, pivotIndex-1, Math.floor((pivotIndex-1+leftIndex)/2));
+    sortSubarray(list, ascending, sb, pivotIndex+1, rightIndex, Math.floor((pivotIndex+1+rightIndex)/2));
   }
 
-  return function quickSort(l, asc){
-    actions = [];
-    step = 0;
-    list = l;
-    ascending = (asc === undefined) ? true : asc;
+  return function quickSort(list, ascending){
+    sb = new StepBuilder();
 
-    // sort(0, list.length-1, Math.floor(Math.random()*list.length));
-    sortSubarray(0, list.length-1, 3);
-    return actions;
+    // TODO change to random pivot
+    sortSubarray(list, ascending, sb, 0, list.length-1, 3);
+
+    return sb.steps;
   };
 });
