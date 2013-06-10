ef005c3b71424cfbf92e3ca78a12a60066ba50dd
OliverCardoza <oracardo@gmail.com>
2013-06-10 11:17:25 -0400

    Fixed merge sort algorithm. TODO: graphic

--- scripts/algorithms/mergeSort.js
+++ scripts/algorithms/mergeSort.js
@@ -1,17 +1,42 @@
 define(['step/StepBuilder'],
 function(StepBuilder){
-  function sortSubarray(list, ascending, sb, leftIndex, rightIndex){
-    console.log('sorting: ', leftIndex, '-', rightIndex);
-    if(leftIndex === rightIndex){
-    } else if (leftIndex-rightIndex === 1){
-      var outOfOrder = (ascending && list[leftIndex]>list[rightIndex]) || (!ascending && list[leftIndex]<list[rightIndex]);
-      if(outOfOrder){
-        sb.swap(list, leftIndex, rightIndex);
+  // in-place combination of 2 sorted subarrays
+  function combineSubarrays(list, ascending, sb, leftIndex, pivotIndex, rightIndex){
+    var subarray = list.slice(leftIndex, rightIndex+1);
+    var leftPtr = 0;
+    var rightPtr = pivotIndex-leftIndex+1;
+    var currentIndex = leftIndex;
+
+    while (currentIndex<rightIndex+1) {
+      var pickLeft = true;
+      // pick right if finished left subarray
+      if (leftPtr>(pivotIndex-leftIndex)) {
+        pickLeft = false;
+      // pick right if ascending order and right<left
+      } else if (ascending && subarray[leftPtr]>subarray[rightPtr]) {
+        pickLeft = false;
+      } else if (!ascending && subarray[leftPtr]<subarray[rightPtr]) {
+        pickLeft = false;
       }
-    } else {
+
+      if (pickLeft) {
+        list[currentIndex] = subarray[leftPtr];
+        leftPtr++;
+      } else {
+        list[currentIndex] = subarray[rightPtr];
+        rightPtr++;
+      }
+      currentIndex++;
+    }
+  }
+
+  // ignore if subarray length=1, break up and recombine if length>2
+  function sortSubarray(list, ascending, sb, leftIndex, rightIndex){
+    if(leftIndex !== rightIndex){
       var pivotIndex = Math.floor((leftIndex+rightIndex)/2);
       sortSubarray(list, ascending, sb, leftIndex, pivotIndex);
       sortSubarray(list, ascending, sb, pivotIndex+1, rightIndex);
+      combineSubarrays(list, ascending, sb, leftIndex, pivotIndex, rightIndex)
     }
   }
 
@@ -19,30 +44,6 @@ function(StepBuilder){
     var sb = new StepBuilder();
 
     sortSubarray(list, ascending, sb, 0, list.length-1);
-
     return sb.steps;
   };
 });
-
-// define(['step/StepBuilder'],
-// function(StepBuilder){
-//   return function sort(list, ascending){
-//     var sb = new StepBuilder();
-
-//     for(var outer=0; (outer<list.length-1) && sb.select(list[outer], 'primary'); outer++){
-//       for(var inner=outer+1; (inner<list.length) && sb.select(list[inner], 'secondary'); inner++){
-//         sb.incrementStep();
-//         var outOfOrder = (ascending && list[outer]>list[inner]) || (!ascending && list[outer]<list[inner]);
-//         if(outOfOrder){
-//           sb.swap(list, outer, inner);
-//           sb.incrementStep()
-//         }
-//       }
-//       sb.selectSorted(list[outer]);
-//     }
-//     sb.selectSorted(list[list.length-1]);
-//     sb.deselect('primary');
-//     sb.deselect('secondary');
-//     return sb.steps;
-//   }
-// });
